e808a3b609dd557f52f6cd7fddbd180770b9608c
Niansong Zhang <nz264@cornell.edu>
2023-06-16 15:14:57 -0400

    extract affine map

--- mlir/lib/Conversion/TritonToAIRPass.cpp
+++ mlir/lib/Conversion/TritonToAIRPass.cpp
@@ -10,7 +10,10 @@
 
 #include "air/Conversion/TritonToAIRPass.h"
 #include "air/Dialect/AIR/AIRDialect.h"
+#include "triton/Dialect/Triton/IR/Types.h.inc"
+#include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/Triton/IR/Ops.h.inc"
+#include "mlir/Dialect/Arith/IR/Arith.h"
 
 #include "air/Util/Util.h"
 
@@ -30,6 +33,31 @@ using namespace xilinx::air;
 namespace {
 
 
+void analyzeLoadOp(func::FuncOp func) {
+  // get all tt.get_program_id operations
+  SmallVector<mlir::triton::GetProgramIdOp, 4> getProgramIdOps;
+  func.walk([&](mlir::triton::GetProgramIdOp op) {
+    getProgramIdOps.push_back(op);
+  });
+  // find use-def chains for each get_program_id that ends with tt.splat
+  for (auto getProgramIdOp : getProgramIdOps) {
+    // create an affine expression for the index
+    auto idExpr = getAffineDimExpr(0, func.getContext());
+    for (auto user : getProgramIdOp.getResult().getUsers()) {
+      // if the user is a multiply op
+      if (isa<arith::MulIOp>(user)) {
+        if (auto constOp = user->getOperands()[1].getDefiningOp<arith::ConstantOp>()) {
+          auto constVal = constOp.getValue().cast<IntegerAttr>().getInt();
+          auto constExpr = getAffineConstantExpr(constVal, func.getContext());
+          auto affineMap = AffineMap::get(1, 0, idExpr * constExpr);
+          llvm::outs() << "affine map: " << affineMap << "\n";
+        }
+      }
+    }
+  }
+}
+
+
 
 class TritonToAIRPass : public TritonToAIRBase<TritonToAIRPass> {
 
@@ -38,9 +66,12 @@ public:
   TritonToAIRPass(const TritonToAIRPass &pass) {}
 
   void runOnOperation() override {
-
     auto module = getOperation();
     auto context = module.getContext();
+    // get all functions
+    for (auto func : module.getOps<func::FuncOp>()) {
+      analyzeLoadOp(func);
+    }
   }
 };
 
